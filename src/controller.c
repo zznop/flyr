a896b219dff0b1ca885a7516ea509a18bd2a6f7c
zznop <zznop0x90@gmail.com>
2018-09-09 01:57:22 -0400

    logic to read output params from json file and set them in the controller interface

--- src/controller.c
+++ src/controller.c
@@ -2,7 +2,6 @@
  * controller.c
  */
 
-#include "parson/parson.h"
 #include "utils.h"
 #include "controller.h"
 
@@ -20,6 +19,11 @@ static uint8_t *_raw_data = NULL;
  * Size of raw data buffer
  */
 static size_t _raw_data_size = 0;
+
+/**
+ * Output parameters
+ */
+static struct output_params *_output_params;
 
 /**
  * Read the inline hex string from the file and write it to _raw_data as a bytearray
@@ -61,16 +65,16 @@ static int _consume_inline_data(struct json_value_t *json_input_value)
 }
 
 /**
- * Parse input value and consume data based on parameters
+ * Parse input value and set data based on parameters
  */
-static int _consume_input(void)
+static int _set_input_params(void)
 {
     struct json_value_t *json_input_value = NULL;
     const char *method = NULL;
     int ret = FAILURE;
 
     if (_json_root_value == NULL) {
-        return FAILURE;
+        goto done;
     }
 
     json_input_value = json_object_get_value(json_object(_json_root_value), "input");
@@ -89,7 +93,7 @@ static int _consume_input(void)
             goto done;
         }
 
-        dudinfo("%lu bytes of input data has been consumed", _raw_data_size);
+        dudinfo("%lu bytes of input data consumed", _raw_data_size);
     } else {
         duderr("unsupported input method: %s", method);
         goto done;
@@ -99,7 +103,96 @@ done:
     if (json_input_value) {
         json_value_free(json_input_value);
     }
+
     return ret;
+}
+
+/**
+ * Parse file output parameters and set _output_params to use them
+ */
+static int _set_file_out_params(struct json_value_t *json_output_value)
+{
+    const char *directory_path = NULL;
+    const char *name_suffix = NULL;
+    struct file_out_params *fout_params = NULL;
+
+    directory_path = json_object_get_string(json_object(json_output_value), "directory-path");
+    if (!directory_path) {
+        duderr("Export directory path not supplied: \"directory-path\"");
+        return FAILURE;
+    }
+
+    name_suffix = json_object_get_string(json_object(json_output_value), "name-suffix");
+    if (!name_suffix) {
+        duderr("Name suffix for exported files not supplied: \"name-suffix\"");
+        return FAILURE;
+    }
+
+    fout_params = (struct file_out_params *)malloc(sizeof(struct file_out_params));
+    if (!fout_params) {
+        goto fail;
+    }
+
+    fout_params->directory_path = directory_path;
+    fout_params->name_suffix = name_suffix;
+
+    _output_params = (struct output_params *)malloc(sizeof(struct output_params));
+    if (!_output_params) {
+        goto fail;
+    }
+
+    _output_params->method = OUTPUT_FILEOUT;
+    _output_params->json_output_value = json_output_value;
+    _output_params->params = fout_params;
+
+    return SUCCESS;
+
+fail:
+    duderr("Out of memory");
+    if (fout_params) {
+        free(fout_params);
+    }
+
+    return FAILURE;
+}
+
+/**
+ * Parse output method from JSON file and set parameters
+ */
+static int _set_output_params(void)
+{
+    struct json_value_t *json_output_value = NULL;
+    const char *method = NULL;
+
+    if (_json_root_value == NULL) {
+        return FAILURE;
+    }
+
+    json_output_value = json_object_get_value(json_object(_json_root_value), "output");
+    if (!json_output_value) {
+        duderr("failed to parse JSON output value");
+        return FAILURE;
+    }
+
+    method = json_object_get_string(json_object(json_output_value), "method");
+    if (!method) {
+        duderr("output method was not specified");
+        return FAILURE;
+    }
+
+    if (!strcmp(method, "file-out")) {
+       if (_set_file_out_params(json_output_value)) {
+           return FAILURE;
+       } 
+
+       dudinfo("output parameters set to export files of suffix %s to directory path %s",
+              _output_params->params->name_suffix, _output_params->params->directory_path);
+    } else {
+        duderr("unsupported export method: %s", method);
+        return FAILURE;
+    }
+
+    return SUCCESS;
 }
 
 /**
@@ -131,10 +224,16 @@ int parse_dudley_file(const char *filepath)
     }
 
     name = json_object_get_string(json_object(_json_root_value), "name");
-    dudinfo("%s (%s) loaded successfully", filepath, name);
+    dudinfo("%s loaded successfully!", filepath, name);
+    printf("  -- NAME: %s\n", name);
 
-    if (_consume_input()) {
-        duderr("Failed to parse and initialize the input method");
+    if (_set_input_params()) {
+        duderr("Failed to parse and initialize the input parameters");
+        goto done;
+    }
+
+    if (_set_output_params()) {
+        duderr("Failed to parse and initialize the output parameters");
         goto done;
     }
 
--- src/controller.h
+++ src/controller.h
@@ -4,7 +4,24 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include "parson/parson.h"
 
 int parse_dudley_file(const char *filepath);
+
+enum output_method {
+    OUTPUT_UNSPECIFIED = 0,
+    OUTPUT_FILEOUT
+};
+
+struct output_params {
+    enum output_method method;
+    struct json_value_t *json_output_value;
+    struct file_out_params *params;
+};
+
+struct file_out_params {
+    const char *directory_path;
+    const char *name_suffix;
+};
 
 #endif
