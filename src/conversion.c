9a2186ff58e0db2eaffcd3a296f93df0441d7c18
zznop <zznop0x90@gmail.com>
2018-10-07 01:01:38 -0400

    Added a consumer block for qword

--- src/conversion.c
+++ src/conversion.c
@@ -9,34 +9,26 @@
 
 #include "conversion.h"
 
-long hex_string_to_long(const char *hexstr)
+long hexstr_to_long(const char *hexstr)
 {
-    long val;
-
-    if (!hexstr)
-        return -1;
-
-    val = strtol(hexstr, NULL, 0);
-    if ((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN)) ||
-        (errno != 0 && val == 0)) {
-        return -1;
+    if (!hexstr) {
+        errno = EFAULT;
+        return 0;
     }
 
-    return val;
+    return strtol(hexstr, NULL, 0);
 }
 
-uint32_t hex_string_to_dword(const char *hexstr, enum endianess endian)
+uint32_t hexstr_to_dword(const char *hexstr, enum endianess endian)
 {
     uint32_t val;
 
-    if (!hexstr)
-        return -1;
-
-    val = strtoul(hexstr, NULL, 0);
-    if ((errno == ERANGE) || (errno != 0 && val == 0)) {
-        return -1;
+    if (!hexstr) {
+        errno = EFAULT;
+        return 0;
     }
 
+    val = strtoul(hexstr, NULL, 0);
     if (endian == BIG_ENDIAN) {
         return __bswap_32(val);
     }
@@ -44,23 +36,38 @@ uint32_t hex_string_to_dword(const char *hexstr, enum endianess endian)
     return val;
 }
 
-uint16_t hex_string_to_word(const char *hexstr, enum endianess endian)
+uint16_t hexstr_to_word(const char *hexstr, enum endianess endian)
 {
     uint32_t valdword;
     uint16_t valword;
 
-    if (!hexstr)
-        return -1;
-
-    valdword = strtoul(hexstr, NULL, 0);
-    if ((errno == ERANGE) || (errno != 0 && valdword == 0)) {
-        return -1;
+    if (!hexstr) {
+        errno = EFAULT;
+        return 0;
     }
 
+    valdword = strtoul(hexstr, NULL, 0);
     valword = (uint16_t)valdword;
     if (endian == BIG_ENDIAN) {
         return __bswap_16(valword);
     }
 
     return valword;
+}
+
+uint64_t hexstr_to_qword(const char *hexstr, enum endianess endian)
+{
+    uint64_t qword;
+    char *end;
+
+    if (!hexstr) {
+        errno = EFAULT;
+        return 0;
+    }
+
+    qword = strtoull(hexstr, &end, 0);
+    if (endian == BIG_ENDIAN)
+        return __bswap_64(qword);
+
+    return qword;
 }
