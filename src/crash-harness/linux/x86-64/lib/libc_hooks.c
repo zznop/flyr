b9f6be03c9ca96e334c79f01767485850e022930
zznop <zznop0x90@gmail.com>
2018-12-01 13:37:43 -0500

    Dynamically pushing and popping tagged allocations to linked list

--- src/crash-harness/linux/x86-64/lib/libc_hooks.c
+++ src/crash-harness/linux/x86-64/lib/libc_hooks.c
@@ -15,18 +15,95 @@ typedef uint64_t tag_t;
 typedef void *(*malloc_t)(size_t size);
 typedef void (*free_t)(void *ptr);
 
+struct alloc_info {
+    void *base;
+    size_t size;
+    struct alloc_info* next;
+};
+
+struct alloc_info *_tagged_allocs;
+
+/**
+ * Lookup if an allocation is tagged, or not
+ */
+int is_tagged_allocation(void *ptr)
+{
+    struct alloc_info *curr;
+    curr = _tagged_allocs;
+    while (curr != NULL) {
+        if (curr->base == ptr)
+            return 1;
+
+        curr = curr->next;
+    }
+
+    return 0;
+}
+
+/**
+ * Push a tagged allocation to the end of the linked list
+ */
+void push_alloc(struct alloc_info *alloc)
+{
+    struct alloc_info *curr;
+    if (!_tagged_allocs) {
+        _tagged_allocs = alloc;
+        return;
+    }
+
+    curr = _tagged_allocs;
+    while (curr->next != NULL)
+        curr = curr->next;
+
+    curr->next = alloc;
+}
+
+void pop_alloc(void *ptr, free_t free_real)
+{
+    struct alloc_info *curr, *prev;
+   
+    curr = _tagged_allocs;
+    while (curr != NULL) {
+        if (curr->base != ptr)
+            goto next;
+
+        // First alloc in list?
+        if (curr == _tagged_allocs) {
+
+            // Increment _tagged_allocs base if next is populated,
+            // otherwise set base to NULL
+            if (curr->next  != NULL)
+                _tagged_allocs = curr->next;
+            else
+                _tagged_allocs = NULL;
+
+            free_real(curr);
+            break;
+        }
+
+        prev->next = curr->next;
+        free_real(curr);
+        break;
+next:
+        prev = curr;
+        curr = curr->next;
+    }
+}
+
 /**
  * Decrement the pointer before the tag so we can free at the correct pointer
  */
 void free(void *ptr)
 {
-    free_t free_real;
-
-    free_real = (free_t)dlsym(RTLD_NEXT, "free");
+    free_t free_real = (free_t)dlsym(RTLD_NEXT, "free");
     if (!free_real)
         FAIL();
 
-    free_real(ptr - sizeof(tag_t));
+    if (is_tagged_allocation(ptr - sizeof(tag_t))) {
+        free_real(ptr - sizeof(tag_t));
+    } else {
+        free_real(ptr);
+    }
 }
 
 /**
@@ -37,6 +114,7 @@ void *malloc(size_t size)
     malloc_t malloc_real; 
     size_t new_size;
     uint8_t *ptr;
+    struct alloc_info *alloc;
 
     malloc_real = (malloc_t)dlsym(RTLD_NEXT, "malloc");
     if (!malloc_real)
@@ -47,7 +125,20 @@ void *malloc(size_t size)
     if (!ptr)
         return NULL;
 
+    // Apply the tags
     *(uint64_t *)ptr = TAG_VAL;
     *(uint64_t *)(ptr + new_size - sizeof(tag_t)) = TAG_VAL;
+
+    // Cache the allocation info
+    alloc = malloc_real(sizeof(*alloc));
+    if (!alloc)
+        FAIL();
+
+    alloc->base = ptr;
+    alloc->size = new_size;
+    alloc->next = NULL;
+    push_alloc(alloc);
+
+    // Return as if base is after the tag
     return ptr + sizeof(tag_t);
 }
