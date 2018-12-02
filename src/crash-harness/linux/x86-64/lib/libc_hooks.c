b209f8c6218cec6638b0ec19694eafae965e7706
zznop <zznop0x90@gmail.com>
2018-12-02 01:12:43 -0500

    Have a proof-of-concept working for detecting OOB writes

--- src/crash-harness/linux/x86-64/lib/libc_hooks.c
+++ src/crash-harness/linux/x86-64/lib/libc_hooks.c
@@ -3,17 +3,22 @@
 #include <dlfcn.h>
 #include <stdint.h>
 #include <pthread.h>
-
-#define TAG_VAL 0x666C7972666C7972UL
-#define LIBC_PATH "/lib/x86_64-linux-gnu/libc.so.6"
-#define __debug_break() __asm__ volatile("int $0x03");
-
-#define FAIL() \
-    exit(1);
+#include <stdarg.h>
 
 typedef uint64_t tag_t;
 typedef void *(*malloc_t)(size_t size);
 typedef void (*free_t)(void *ptr);
+typedef void *(*memcpy_t)(void *destination, const void *source, size_t num);
+typedef int (*printf_t)(const char *format, ...);
+typedef int (*vsnprintf_t)(char *s, size_t n, const char *format, va_list arg);
+
+#define TAG_VAL 0x666C7972666C7972UL
+#define LIBC_PATH "/lib/x86_64-linux-gnu/libc.so.6"
+#define BUG() __asm__ volatile(".int 0xdeadc0de");
+#define LOG_BUF_SIZE (256)
+
+#define FAIL() \
+    BUG()
 
 struct alloc_info {
     void *base;
@@ -21,7 +26,27 @@ struct alloc_info {
     struct alloc_info* next;
 };
 
+/**
+ * Globals
+ */
+
 struct alloc_info *_tagged_allocs;
+
+/**
+ * Debug print
+ */
+void info(const char *fmt, ...)
+{
+    va_list ap;
+    char buf[LOG_BUF_SIZE];
+    vsnprintf_t real_vsnprintf = (vsnprintf_t)dlsym(RTLD_NEXT, "vsnprintf");
+    printf_t real_printf = (printf_t)dlsym(RTLD_NEXT, "printf");
+
+    va_start(ap, fmt);
+    real_vsnprintf(buf, LOG_BUF_SIZE, fmt, ap);
+    va_end(ap);
+    real_printf("\033[22;34m%s\033[0m\n", buf);
+}
 
 /**
  * Lookup if an allocation is tagged, or not
@@ -58,6 +83,9 @@ void push_alloc(struct alloc_info *alloc)
     curr->next = alloc;
 }
 
+/**
+ * Remove a free'd allocation from the tagged allocation list
+ */
 void pop_alloc(void *ptr, free_t free_real)
 {
     struct alloc_info *curr, *prev;
@@ -91,11 +119,55 @@ next:
 }
 
 /**
- * Decrement the pointer before the tag so we can free at the correct pointer
+ * Check if any tags have been overwritten
+ */
+static void check_tagged_allocs(void)
+{
+    struct alloc_info *curr;
+
+    curr = _tagged_allocs;
+    while (curr != NULL) {
+        if ((uint64_t)(*(uint64_t *)curr->base) != TAG_VAL ||
+            (uint64_t)(*(uint64_t *)(curr->base + curr->size - sizeof(TAG_VAL)) != TAG_VAL)) {
+            info("\n############################################################");
+            info("# OOB write detected!!!");
+            info("#  -- alloc base: %llx", curr->base - sizeof(TAG_VAL));
+            info("#  -- alloc size: %u", curr->size - sizeof(TAG_VAL) * 2);
+            info("############################################################\n");
+            BUG();
+        }
+
+        curr = curr->next;
+    }
+}
+
+/**
+ * Check for OOB writes
+ */
+void *memcpy (void *destination, const void *source, size_t num)
+{
+    void *ret;
+    memcpy_t memcpy_real;
+
+    check_tagged_allocs();
+    memcpy_real = (memcpy_t)dlsym(RTLD_NEXT, "memcpy");
+    if (!memcpy_real)
+        FAIL();
+
+    ret = memcpy_real(destination, source, num);
+
+    return ret;
+}
+
+/**
+ * Hook free to prevent crashes while freeing tagged allocations
  */
 void free(void *ptr)
 {
-    free_t free_real = (free_t)dlsym(RTLD_NEXT, "free");
+    free_t free_real;
+
+    check_tagged_allocs();
+    free_real = (free_t)dlsym(RTLD_NEXT, "free");
     if (!free_real)
         FAIL();
 
@@ -120,6 +192,7 @@ void *malloc(size_t size)
     if (!malloc_real)
         FAIL();
   
+    check_tagged_allocs();
     new_size = size + sizeof(tag_t) * 2;
     ptr = malloc_real(new_size);
     if (!ptr)
